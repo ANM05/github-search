5eafb9f0066f7231abae9cbf2cebb46ce8939aa2
Antony Nganga <antonynganga05@gmail.com>
2019-03-30 18:56:08 +0300

    Adds http profile service that handles http requests

--- src/app/profile-http/profile-request.service.ts
+++ src/app/profile-http/profile-request.service.ts
@@ -3,10 +3,45 @@ import {HttpClient} from '@angular/common/http';
 import {Users} from '../user-class/users';
 import {environment} from '../../environments/environment';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileRequestService {
+  user:Users;
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.user= new Users("","",0,0,0);
+   }
+
+   profileRequest(){
+    interface ProfileResponse{
+      login:string;
+      avatar_url:any;
+      public_repos:number;
+      followers:number;
+      following:number;
+    }
+
+    let promise=new Promise((resolve,reject)=>{
+      this.http.get<ProfileResponse>(environment.apiUrl).toPromise().then(response=>{
+        this.user.login=response.login;
+        this.user.avatar_url=response.avatar_url;
+        this.user.public_repos=response.public_repos;
+        this.user.followers=response.followers;
+        this.user.following=response.following;
+
+        resolve();
+      },
+      error=>{
+        alert("An Error has occurred getting your request");
+
+        reject(error);
+      }
+      
+      )
+
+    });
+    return promise;
+   }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,32 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 import {Users} from '../user-class/users';
+import {ProfileRequestService} from '../profile-http/profile-request.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
+  providers:[ProfileRequestService],
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
   user:Users;
 
-  constructor(private http:HttpClient) { }
+  constructor(private profileService:ProfileRequestService) { }
 
   ngOnInit() {
-    interface ProfileResponse{
-      login:string;
-      avatar_url:any;
-      public_repos:number;
-      followers:number;
-      following:number;
-    }
-
-    this.http.get<ProfileResponse>("https://api.github.com/users/anm05?access_token=852254f32dd6f78719a0630b5ef4ddf9eaf041f3").subscribe(response=>{
-      this.user= new Users(response.login,response.avatar_url, response.public_repos, response.followers, response.following)
-      //successfull request
-    },err=>{
-      console.log("An error has occured");
-    })
+    this.profileService.profileRequest()
+    this.user = this.profileService.user  
   }
 
 }
